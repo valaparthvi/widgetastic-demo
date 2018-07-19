c20a026e54d1c51624b1ee9bf2c6f64c31fa4aba
Parthvi Vala <pvala@redhat.com>
2018-07-19 15:59:48 +0530

    Working model ready

--- widgetastic_demo/forms.py
+++ widgetastic_demo/forms.py
@@ -2,18 +2,18 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    fullname = forms.CharField(max_length=50)
-    username = forms.CharField(max_length=20)
-    email = forms.EmailField()
-    dob = forms.DateField()
+    fullname = forms.CharField(max_length=50, required=False)
+    username = forms.CharField(max_length=20, required=False)
+    email = forms.EmailField(required=False)
+    dob = forms.DateField(required=False)
 
 
 class DynamicForm(forms.Form):
     dropdown = forms.CharField(widget=forms.Select(
-        choices=(('', ''), ('menu_1', 'Form'), ('menu_2', 'Simple Text'))))
+        choices=((' ', ' '), ('menu_1', 'Form'), ('menu_2', 'Text Image'))))
 
 
 class DemoForm(forms.Form):
-    name = forms.CharField(max_length=20)
-    dropdown = forms.CharField(widget=forms.Select(
+    name = forms.CharField(max_length=20, required=False)
+    languages = forms.CharField(widget=forms.Select(
         choices=(('python', 'Python'), ('ruby', 'Ruby'), ('c', 'C'), ('javascript', 'JavaScript'))))
