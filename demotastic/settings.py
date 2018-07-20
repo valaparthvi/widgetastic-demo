0790232ea14e65e2c0884bac44b38e064f357341
Parthvi Vala <pvala@redhat.com>
2018-07-20 10:36:45 +0530

    change static settings

--- demotastic/settings.py
+++ demotastic/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATIC_ROOT = [
     os.path.join(BASE_DIR, "static"),
 ]
